b1dcae82be769fa4473eb7b05709cc3a982da367
suuryaalif <suryarachman@gmail.com>
2022-04-08 20:52:57 +0700

    menambahkan Biodata1.php

---
+++ application/controllers/Biodata1.php
@@ -0,0 +1,11 @@
+<?php
+class Biodata1 extends CI_Controller
+ {public function index()
+
+    {
+    echo "<h1> Perkenalkan Rekan-Rekan </h1>";
+    echo "Nama Saya Surya Alif Rachman
+    Saya Tinggal di Bekasi kegiatan saya sehari hari
+    bekerja dan bermain bersama anak";
+    }    
+}
